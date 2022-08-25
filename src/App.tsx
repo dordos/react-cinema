08c7b2c4c989d967d3293e3c0d89b71e25b43e04
dordos <pdhssf@gmail.com>
2022-08-25 19:15:02 -0400

    .evn test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const test =  axios.get(
+    'https://api.themoviedb.org/3/discover/movie?api_key=<<api_key>>&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
+  )
+console.log("-> test", test);
   return (
     <div className="App">
       <header className="App-header">
