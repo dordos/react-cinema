6b68e6e6f83615ce0a4466b3bcf58d9953e3fe3c
dordos <pdhssf@gmail.com>
2022-09-04 22:00:13 -0400

    axios test

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_CINEMA_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
+  const [data, setData] = useState("");
+  const list = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setData(response.data);
+    } catch (e) {}
+  };
 
-  const test =  axios.get(
-    'https://api.themoviedb.org/3/discover/movie?api_key=<<api_key>>&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
-  )
-console.log("-> test", test);
-  return (
-    <div className="App">
-      <header className="App-header">
+  useEffect(() => {
+    list();
+  }, []);
 
-      </header>
-    </div>
-  );
+  // const [aa, setaa] = useState("");
+  // const moviesData = axios.get().then((response) => {
+  //   setaa(response.data.results[0].original_title);
+  // });
+
+  return <div>{data}</div>;
 }
 
 export default App;
