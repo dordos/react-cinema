100b5d835c292c718c34ef89157297b1af02f361
dordos <pdhssf@gmail.com>
2022-09-16 22:03:02 -0400

    axios test

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,34 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
+// type User = {
+//   id: number;
+//   name: string;
+// };
+
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_CINEMA_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
+
 function App() {
-  const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_CINEMA_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
-  const [data, setData] = useState("");
-  const list = async () => {
-    try {
-      const response = await axios.get(API_URL);
-      setData(response.data);
-    } catch (e) {}
-  };
+  // const [users, setUsers] = useState<User[] | undefined>(undefined);
+  const [test, setTest] = useState("");
+  const a = axios
+    .get(API_URL)
+    .then((response) => {
+      console.log("-> response", response);
+    })
+    .catch((error) => {
+      console.log("-> error", error);
+    });
 
   useEffect(() => {
-    list();
+    // list();
   }, []);
 
-  // const [aa, setaa] = useState("");
-  // const moviesData = axios.get().then((response) => {
-  //   setaa(response.data.results[0].original_title);
-  // });
-
-  return <div>{data}</div>;
+  return (
+    <div>
+      <p>{test}</p>
+    </div>
+  );
 }
 
 export default App;
