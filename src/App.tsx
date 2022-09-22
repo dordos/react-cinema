b06d5543cd75e59eed17df455f56b4aa5f41a9f2
dordos <pdhssf@gmail.com>
2022-09-21 22:21:29 -0400

    item style update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import Posts from "./components/Posts";
+import Movies from "./components/Movies";
 
 // type User = {
 //   id: number;
@@ -10,37 +10,30 @@ import Posts from "./components/Posts";
 
 const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_CINEMA_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
 function App() {
-  const [posts, setPosts] = useState([]);
-  // const [users, setUsers] = useState<User[] | undefined>(undefined);
-  // const [test, setTest] = useState("");
-  // const a = axios
-  //   .get(API_URL)
-  //   .then((response) => {
-  //     response.data.results.map((item: any) => {
-  //       console.log("-> item.release_date", item.release_date);
-  //       setTest(item.release_date);
-  //     });
-  //     // setTest(response.data.results);
-  //   })
-  //   .catch((error) => {
-  //     console.log("-> error", error);
-  //   });
+  const [movieInfo, setMovieInfo] = useState([]);
 
   useEffect(() => {
     const movieData = async () => {
       const response = await axios.get(API_URL);
-      // response.data.results.map((item: any) => {
-      //   setPosts(item);
-      // });
-      setPosts(response.data.results);
+      setMovieInfo(response.data.results);
     };
     movieData();
   }, []);
 
   return (
-    <ul>
-      <Posts posts={posts} />
-    </ul>
+    <Movies movieInfo={movieInfo} />
+
+    // <ul>
+    //   {posts.map((post: any) => (
+    //     <li key={post.id}>
+    //       {post.title}e
+    //       <img
+    //         src={`https://image.tmdb.org/t/p/w500/${post.poster_path}`}
+    //         alt=""
+    //       />
+    //     </li>
+    //   ))}
+    // </ul>
   );
 }
 
---
+++ src/components/Movies/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.scss";
+
+const Movies = ({ movieInfo }: any) => {
+  console.log("-> movieInfo", movieInfo);
+  return (
+    <ul>
+      {movieInfo.map((movie: any) => (
+        <li key={movie.id}>
+          {/*<p>{movie.title}</p>*/}
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+            alt=""
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Movies;
