929761ac476aa86d1fdc6b1624163d8c8f5eee5c
dordos <pdhssf@gmail.com>
2023-01-13 00:52:15 +0900

    login modal update

--- src/components/LogInModal/index.tsx
+++ src/components/LogInModal/index.tsx
@@ -1,29 +1,36 @@
 import React, { useCallback, useState } from 'react';
 import { Form, Link } from 'react-router-dom';
 import SingUp from '../../page/SignUp';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import './style.scss';
+import useInput from '../../hooks/useInput';
 
 const LogInModal = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  // const onSubmit = useCallback(
-  //   (e: React.FormEvent<HTMLInputElement>) => {
-  //     e.preventDefault();
-  //   },
-  //   [email, password]
-  // );
+  const [email, setEmail] = useInput('');
+  const [password, setPassword] = useInput('');
 
+  const onSubmit = useCallback(() => {}, [email, password]);
+
+  const auth = getAuth();
+  const login = () => {
+    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    });
+  };
+  const handler = () => {};
+  const handleLogin = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') login();
+  };
   return (
     <div className='logInModal'>
       {/* <Form onSubmit={onSubmit}> */}
       <div className='logInContainer'>
-        <input type='email' placeholder='이메일' />
-        <input
-          type='password'
-          placeholder='비밀번호'
-          // onChange={setPassword}
-        />
-        <button>로그인</button>
+        <Form onKeyDown={onSubmit}>
+          <input type='email' placeholder='이메일' onChange={setEmail} />
+          <input type='password' placeholder='비밀번호' onChange={setPassword} />
+          <button onClick={login}>로그인</button>
+        </Form>
         <div>
           <p>
             계정이 없으신가요?
