e5c60a895b46c43b8b86c1cfbda4d23d72d50df0
dordos <pdhssf@gmail.com>
2023-01-26 23:20:22 +0900

    movie previw modal update

--- src/page/MoviePreview/index.tsx
+++ src/page/MoviePreview/index.tsx
@@ -11,6 +11,8 @@ const MoviePreview = () => {
     poster_path: string;
     release_date: string;
     overview: string;
+    genres: Array<{ id: number; name: string }>;
+    spoken_languages: Array<{ iso_639_1: string }>;
   };
 
   const [detailData, setDetailData] = useState<movieDetail>();
@@ -36,7 +38,20 @@ const MoviePreview = () => {
           <div className='overview'>
             <p>{detailData?.overview}</p>
           </div>
-          <div className='metaDataDetail'></div>
+          <div className='metaDataDetail'>
+            <div>
+              <p className='language'>
+                지원 언어 :
+                {detailData?.spoken_languages.map((language) => (
+                  <span>{language.iso_639_1}</span>
+                ))}
+              </p>
+              <p className='genres'>
+                <span>장르 : </span>
+                {detailData?.genres.map((item) => `${item.name}, `)}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
