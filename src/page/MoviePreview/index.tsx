e4b6717e101da605bd81126f5efa4c8a44b4bd6b
dordos <pdhssf@gmail.com>
2023-01-27 23:10:05 +0900

    movie preview update

--- src/page/MoviePreview/index.tsx
+++ src/page/MoviePreview/index.tsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './style.scss';
+import cart from '../../img/cart.svg';
+import heart from '../../img/heart.svg';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 const MoviePreview = () => {
   const MOVIE_DETAIL = `https://api.themoviedb.org/3/movie/${505642}?api_key=${
@@ -28,28 +31,44 @@ const MoviePreview = () => {
   return (
     <div className='moviePreviewContainer'>
       <div className='previewContent'>
-        <div className='previewImg'>
+        <div className='previewLeft'>
           <img src={`https://image.tmdb.org/t/p/w500/${detailData?.poster_path}`} alt='' />
         </div>
-        <div>
-          <div className='metaData'>
-            <span>{detailData?.release_date}</span>
+        <div className='previewRight'>
+          <div className='closeBtn'>
+            <AiFillCloseCircle size='36' color='#353535' />
           </div>
-          <div className='overview'>
-            <p>{detailData?.overview}</p>
-          </div>
-          <div className='metaDataDetail'>
-            <div>
-              <p className='language'>
-                지원 언어 :
-                {detailData?.spoken_languages.map((language) => (
-                  <span>{language.iso_639_1}</span>
-                ))}
-              </p>
-              <p className='genres'>
-                <span>장르 : </span>
-                {detailData?.genres.map((item) => `${item.name}, `)}
-              </p>
+          <div className='previewInfo'>
+            <div className='metaData'>
+              <span>{detailData?.release_date}</span>
+            </div>
+
+            <div className='overview'>
+              <p>{detailData?.overview}</p>
+            </div>
+            <div className='metaDataDetail'>
+              <div>
+                <p className='language'>
+                  지원 언어 :
+                  {detailData?.spoken_languages.map((language, idx) => (
+                    <span key={idx}>{language.iso_639_1}</span>
+                  ))}
+                </p>
+                <p className='genres'>
+                  장르 :
+                  {detailData?.genres.map((item, idx) => (
+                    <span key={idx}>{item.name}</span>
+                  ))}
+                </p>
+              </div>
+            </div>
+            <div className='myPageInfo'>
+              <div className='heartWrap'>
+                <img src={heart} alt='' />
+              </div>
+              <div className='cartWrap'>
+                <img src={cart} alt='' />
+              </div>
             </div>
           </div>
         </div>
