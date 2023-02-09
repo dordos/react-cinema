90d3ddd17ce4807819b47141147359a5db1b7bbd
dordos <pdhssf@gmail.com>
2023-02-09 23:41:29 +0900

    PickList page  style update

--- src/page/OrderList/index.tsx
+++ src/page/OrderList/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import MenuBar from '../../components/MenuBar';
+import './style.scss';
 
 const OrderList = () => {
   return (
     <>
       <MenuBar />
+      <div className='orderList'>
+        <div>대여 중인 목록</div>
+        <div>만료된 대여 목록</div>
+      </div>
     </>
   );
 };
--- src/page/PickList/index.tsx
+++ src/page/PickList/index.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import MovieModal from '../../components/MovieModal';
 import MenuBar from '../../components/MenuBar';
 import './style.scss';
+import { BsStar, BsStarHalf, BsStarFill } from 'react-icons/bs';
 
 const PickList = () => {
   const [movieModalState, setMovieModalState] = useState(false);
@@ -13,13 +14,6 @@ const PickList = () => {
   const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=ko-KR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
   const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
-  useEffect(() => {
-    async function movieData() {
-      const response = await axios.get(API_URL);
-      setMovieInfo(response.data.results);
-    }
-    movieData();
-  }, []);
   const closeModal = () => setMovieModalState(false);
 
   const onMovieDetail = (id: any) => {
@@ -28,6 +22,36 @@ const PickList = () => {
     setMovieModalState(!movieModalState);
     setSelectMovie(id);
   };
+
+  const [starAverage, setStarAverage] = useState([
+    <BsStar size='20' color='#3beb12' />,
+    <BsStar size='20' color='#3beb12' />,
+    <BsStar size='20' color='#3beb12' />,
+    <BsStar size='20' color='#3beb12' />,
+    <BsStar size='20' color='#3beb12' />,
+  ]);
+
+  const star = (average: number) => {
+    const [first, second] = ((average / 10) * 5).toFixed(1).split('.');
+    const averageCopy = [...starAverage];
+
+    for (let i = 0; i < Number(first); i++) {
+      averageCopy[i] = <BsStarFill size='20' color='#3beb12' />;
+    }
+    if (Number(second) >= 5) {
+      averageCopy[Number(first)] = <BsStarHalf size='20' color='#3beb12' />;
+    }
+    setStarAverage(averageCopy);
+  };
+
+  useEffect(() => {
+    async function movieData() {
+      const response = await axios.get(API_URL);
+      setMovieInfo(response.data.results);
+      star(response.data.vote_average);
+    }
+    movieData();
+  }, []);
 
   return (
     <>
@@ -41,6 +65,7 @@ const PickList = () => {
             }}
           >
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt='' />
+            <div>{starAverage}</div>
           </li>
         ))}
       </ul>
