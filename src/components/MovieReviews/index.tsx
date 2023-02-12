54738c75f1ef03652bcf3b76cd50666cfcf58b96
dordos <pdhssf@gmail.com>
2023-02-13 00:40:11 +0900

    review layout update

--- src/components/MovieReviews/index.tsx
+++ src/components/MovieReviews/index.tsx
@@ -1,43 +1,162 @@
 import React from 'react';
 import './style.scss';
-import { BsStar, BsStarHalf, BsStarFill } from 'react-icons/bs';
+import { BsStar, BsStarFill } from 'react-icons/bs';
+// import { IoChevronBackOut, IoChevronForward } from 'react-icons/io';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 const Reviews = () => {
+  // const [starAverage, setStarAverage] = useState([
+  // ]);
+
   return (
     <div className='reviews'>
       <div className='addReviews'>
         <div className='moviePoint'>
-          <p>별점을 입력해 주세요.</p>
-          <div>
-            <button>-</button>
-            <span>0</span>
-            <button>+</button>
+          <p>별점을 선택해주세요.</p>
+          <div className='grade'>
+            <BsStar size='20' color='#888888' />
+            <BsStar size='20' color='#888888' />
+            <BsStar size='20' color='#888888' />
+            <BsStar size='20' color='#888888' />
+            <BsStar size='20' color='#888888' />
           </div>
         </div>
         <div className='addReviewBox'>
           <textarea placeholder='감상평을 등록해주세요.' />
-          <button>등록</button>
-        </div>
-        <div>
-          <div className='socialReview'>
-            <div>
-              <BsStar />
-            </div>
-            <div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis maiores non quas
-                incidunt? Dolorum, quisquam, perspiciatis esse tempora saepe et minus rerum non,
-                doloremque officiis ipsum illum nobis sapiente placeat!
-              </p>
-            </div>
-            <div>2022.02.02</div>
-          </div>
+          <button>등록하기</button>
         </div>
       </div>
       <div className='reviewsList'>
-        <div>
-          <p>ddd</p>
-        </div>
+        <ul>
+          <li>
+            <div className='reviewsListNumber'>1</div>
+            <div className='reiviewsListContent'>
+              <div className='movieListPoint'>
+                <div className='grade'>
+                  <BsStarFill size='14' color='#e22232' />
+                  <BsStarFill size='14' color='#e22232' />
+                  <BsStarFill size='14' color='#e22232' />
+                  <BsStarFill size='14' color='#e22232' />
+                  <BsStar size='14' color='#888888' />
+                </div>
+                <div className='reviewScore'>5</div>
+              </div>
+
+              <p className='reviewText'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nesciunt eum
+                laudantium optio vero voluptatum quisquam saepe architecto illum harum porro,
+                mollitia
+              </p>
+              <div>
+                <span className='reviewNickname'>test@test.com</span>
+                <span className='reviewDate'>2022.02.22 08:02</span>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className='reviewsListNumber'>2</div>
+            <div className='reiviewsListContent'>
+              <div className='movieListPoint'>
+                <div className='grade'>
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                </div>
+                <div className='reviewScore'>5</div>
+              </div>
+
+              <p className='reviewText'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nesciunt eum
+                laudantium optio vero voluptatum quisquam saepe architecto illum harum porro,
+                mollitia
+              </p>
+              <div>
+                <span className='reviewNickname'>test@test.com</span>
+                <span className='reviewDate'>2022.02.22 08:02</span>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className='reviewsListNumber'>3</div>
+            <div className='reiviewsListContent'>
+              <div className='movieListPoint'>
+                <div className='grade'>
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                </div>
+                <div className='reviewScore'>5</div>
+              </div>
+              <p className='reviewText'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nesciunt eum
+                laudantium optio vero voluptatum quisquam saepe architecto illum harum porro,
+              </p>
+              <div>
+                <span className='reviewNickname'>test@test.com</span>
+                <span className='reviewDate'>2022.02.22 08:02</span>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className='reviewsListNumber'>4</div>
+            <div className='reiviewsListContent'>
+              <div className='movieListPoint'>
+                <div className='grade'>
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                </div>
+                <div className='reviewScore'>5</div>
+              </div>
+              <p className='reviewText'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nesciunt eum
+                laudantium optio vero voluptatum quisquam saepe architecto illum harum porro,
+              </p>
+              <div>
+                <span className='reviewNickname'>test@test.com</span>
+                <span className='reviewDate'>2022.02.22 08:02</span>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className='reviewsListNumber'>5</div>
+            <div className='reiviewsListContent'>
+              <div className='movieListPoint'>
+                <div className='grade'>
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                  <BsStar size='14' color='#888888' />
+                </div>
+                <div className='reviewScore'>5</div>
+              </div>
+              <p className='reviewText'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nesciunt eum
+                laudantium optio vero voluptatum quisquam saepe architecto illum harum porro,
+              </p>
+              <div>
+                <span className='reviewNickname'>test@test.com</span>
+                <span className='reviewDate'>2022.02.22 08:02</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <ul className='listCount'>
+          <IoIosArrowBack size='18' />
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <IoIosArrowForward size='18' />
+        </ul>
       </div>
     </div>
   );
