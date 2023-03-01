5ebcf32ef4b066c3a5fceb94b730a85ec32e6ba8
dordos <pdhssf@gmail.com>
2023-03-01 19:23:24 +0900

    update react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
+// import './App.css';
+// import { Outlet } from 'react-router-dom';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import React, { useEffect, useState } from 'react';
+
+// // const ReactQueryDevtoolsProduction = React.lazy(() =>
+// //   import('@tanstack/react-query-devtools/build/lib/index.prod.js').then((d) => ({
+// //     default: d.ReactQueryDevtools,
+// //   }))
+// // );
+
+// function App() {
+//   // const [showDevtools, setShowDevtools] = React.useState(false);
+//   // React.useEffect(() => {
+//   //   //@ts-ignore
+//   //   window.toggleDevtools = () => setShowDevtools((old) => !old);
+//   // }, []);
+//   return <Outlet />;
+// }
+
+// export default App;
+
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React, { useEffect, useState } from 'react';
-
-const queryClient = new QueryClient();
-
-const ReactQueryDevtoolsProduction = React.lazy(() =>
-  import('@tanstack/react-query-devtools/build/lib/index.prod.js').then((d) => ({
-    default: d.ReactQueryDevtools,
-  }))
-);
 
 function App() {
-  const [showDevtools, setShowDevtools] = useState(false);
-  useEffect(() => {
-    window.toggleDevtools = () => setShowDevtools((old) => !old);
-  }, []);
   return (
-    <QueryClientProvider client={queryClient}>
-      <Outlet />
-      <ReactQueryDevtools initialIsOpen />
-      {showDevtools && (
-        <React.Suspense>
-          <ReactQueryDevtoolsProduction />
-        </React.Suspense>
-      )}
-    </QueryClientProvider>
+    <>
+      <ReactQueryDevtools initialIsOpen={true} />
+      {/* <Outlet /> */}
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,44 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+// import SignUp from './page/SignUp';
+// import SignIn from './page/SignIn';
+// import MovieDetail from './page/MovieDetail';
+// import PickList from './page/PickList';
+// import Cart from './page/Cart';
+// import OrderList from './page/OrderList';
+// import Home from './page/Home';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// const queryClient = new QueryClient();
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         index: true,
+//         path: '/',
+//         element: <Home />,
+//       },
+//       { path: 'SignUp', element: <SignUp /> },
+//       { path: 'SignIn', element: <SignIn /> },
+//       { path: 'MovieDetail', element: <MovieDetail /> },
+//       { path: 'PickList', element: <PickList /> },
+//       { path: 'Cart', element: <Cart /> },
+//       { path: 'OrderList', element: <OrderList /> },
+//     ],
+//   },
+// ]);
+
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(
+//   <QueryClientProvider client={queryClient}>
+//     <RouterProvider router={router} />
+//     <ReactQueryDevtools initialIsOpen={false} />
+//   </QueryClientProvider>
+// );
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,6 +50,9 @@ import PickList from './page/PickList';
 import Cart from './page/Cart';
 import OrderList from './page/OrderList';
 import Home from './page/Home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -31,4 +75,8 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<RouterProvider router={router} />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
