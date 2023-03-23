46bdc8c179d5bc48f4b665865d17a4bfa13b3fc4
dordos <pdhssf@gmail.com>
2023-03-23 22:32:29 +0900

    modal ui update

--- src/components/ModalCartAlert/index.tsx
+++ src/components/ModalCartAlert/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './style.scss';
-import { AiOutlineCheck } from 'react-icons/ai';
+import { HiCheckCircle } from 'react-icons/hi';
 
 const ModalCartAlert = () => {
   return (
     <div className='modalCartAlert'>
       <div>
         <div>
-          <AiOutlineCheck />
+          <HiCheckCircle />
         </div>
         <h2>장바구니에 추가 되었습니다.</h2>
       </div>
