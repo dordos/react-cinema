8308909f01dd15818581c2836a2498de45c0e8e8
dordos <pdhssf@gmail.com>
2023-03-28 19:21:02 +0900

    make payment ui

--- src/components/PaymentWindow/index.tsx
+++ src/components/PaymentWindow/index.tsx
@@ -3,8 +3,16 @@ import './style.scss';
 
 const PaymentWindow = () => {
   return (
-    <div>
-      <h1>d</h1>
+    <div className='paymentContainer'>
+      <div className='paymentWrap'>
+        <div className='payment__text'>
+          <h2>결제하시겠습니까?</h2>
+        </div>
+        <div className='payment__button'>
+          <button>결제</button>
+          <button>취소</button>
+        </div>
+      </div>
     </div>
   );
 };
