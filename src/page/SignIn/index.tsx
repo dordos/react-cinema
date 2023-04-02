14941dedb6ac717caf41899cd6fe0bf94ce26b92
dordos <pdhssf@gmail.com>
2023-04-02 16:55:05 +0900

    sign up update

--- src/page/SignIn/index.tsx
+++ src/page/SignIn/index.tsx
@@ -1,10 +1,25 @@
 import React, { useCallback, useRef, useState } from 'react';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useNavigation } from 'react-router-dom';
+import { logIn } from '../../api/firebase';
 import useInput from '../../hooks/useInput';
 import './style.scss';
 
 const logo = require('../../img/logo.png');
 const SignIn = () => {
+  const navigate: any = useNavigation();
+  const [email, setEmail] = useInput('');
+  const [password, setPassword] = useInput('');
+
+  const onSubmit = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      logIn(email, password).then(() => {
+        navigate('/');
+      });
+    },
+    [email, password]
+  );
+
   // focus Event
   const focusIn = (e: any) => {
     if (e.target.previousSibling.className == 'inputOffFocus' && e.target.value == '')
@@ -13,7 +28,6 @@ const SignIn = () => {
   const focusOut = (e: any) => {
     if (e.target.value == '') {
       e.target.previousSibling.className = 'inputOffFocus';
-      // e.target.nextSibling.className = 'errorMessage';
     }
   };
 
@@ -28,23 +42,23 @@ const SignIn = () => {
           <h1>Sign In</h1>
         </div>
 
-        <Form>
+        <form onSubmit={onSubmit}>
           <div className='infoContainer'>
             <label onFocus={focusIn} onBlur={focusOut}>
               <span className='inputOffFocus'>이메일</span>
-              <input type='email' name='email' />
+              <input type='email' name='email' onChange={setEmail} />
               <p>규칙에 맞는 이메일 주소를 입력해주세요</p>
             </label>
 
             <label onFocus={focusIn} onBlur={focusOut}>
               <span className='inputOffFocus'>비밀번호</span>
-              <input type='password' name='password' />
+              <input type='password' name='password' onChange={setPassword} />
               <p>비밀번호는 8자리 이상으로 입력해주세요.</p>
             </label>
 
-            <button>로그인</button>
+            <button type='submit'>로그인</button>
           </div>
-        </Form>
+        </form>
 
         <div className='suggestion'>
           <p>
--- src/page/SignUp/index.tsx
+++ src/page/SignUp/index.tsx
@@ -1,52 +1,55 @@
-import React, { ChangeEvent, FormEvent, useCallback, useEffect, useRef, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useCallback, useState } from 'react';
 import { Form, Link } from 'react-router-dom';
-import { User } from '@firebase/auth';
-import useInput from '../../hooks/useInput';
 import './style.scss';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../api/firebase';
-import { EventEmitter } from 'stream';
-import { stringLength } from '@firebase/util';
 import SignIn from '../SignIn';
 const logo = require('../../img/logo.png');
 const checkImg = require('../../img/check_icon.png');
 const SignUp = () => {
   const [email, setEmail] = useState('');
-  const [nickname, setNickname] = useState('');
+  // const [nickname, setNickname] = useState('');
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
-  const [mismatchError, setMismatchError] = useState(false);
+  const [isFocused, setIsFocused] = useState({
+    email: false,
+    password: false,
+    passwordCheck: false,
+  });
+  // const [mismatchError, setMismatchError] = useState(false);
+  // const [passwordLengthError, setPasswordLengthError] = useState(false);
   const onChangeEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   }, []);
 
-  const onChangeNickname = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setNickname(e.target.value);
-  }, []);
+  const isPasswordError = !isFocused.password && password.length <= 6;
+  const isEmailError = !isFocused.email && !/^\S+@\S+\.\S+$/.test(email.trim());
+  const isPasswordCheckError = !isFocused.passwordCheck && passwordCheck !== password;
 
-  const onChangePassword = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setPassword(e.target.value);
-      setMismatchError(e.target.value === passwordCheck);
-    },
-    [setPasswordCheck]
-  );
+  // const onChangeNickname = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+  //   setNickname(e.target.value);
+  // }, []);
+
+  // const onChangePassword = useCallback(
+  //   (e: ChangeEvent<HTMLInputElement>) => {
+  //     setPassword(e.target.value);
+  //     setMismatchError(e.target.value === passwordCheck);
+  //   },
+  //   [setPasswordCheck, setPasswordLengthError]
+  // );
+
+  const passwordLength = (event: React.ChangeEvent<any>) => {
+    const value = event.target.value;
+    if (value.length <= 6) {
+      setPassword(value);
+    } else {
+      setPassword(value);
+    }
+  };
 
   const onChangePasswordCheck = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setPasswordCheck(e.target.value);
   }, []);
-
-  // async function onSubmit() {
-  //   // e.preventDefault();
-  //   return createUserWithEmailAndPassword(auth, email, password)
-  //     .then((userCredential) => {
-  //       const user = userCredential.user;
-  //       console.log(user);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }
 
   const [signUpSuccess, setSignUpSuccess] = useState(false);
   const [signUpPage, setSignPage] = useState(true);
@@ -66,41 +69,29 @@ const SignUp = () => {
     },
     [auth, email, password]
   );
-  const [signUpState, setSignUpState] = useState();
-
-  // async function onSubmit() {
-  //   return createUserWithEmailAndPassword(auth, email, password)
-  //     .then((userCredential) => {
-  //       const user = userCredential.user;
-  //       console.log(user);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }
-
-  // const handleLogin = () => {
-  //   // onSubmit().then(setSignUpState);
-  // };
-
-  const [passwordError, setPasswordError] = useState(false);
-  //이메일 정규식
-  const regEmail = (data: any) => {
-    const regex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
-    return regex.test(data);
-  };
 
   // focus Event
   const focusIn = (e: any) => {
-    if (e.target.previousSibling.className == 'inputOffFocus' && e.target.value == '')
-      e.target.previousSibling.className = 'inputOnFocus';
+    // if (e.target.previousSibling.className == 'inputOffFocus' && e.target.value == '')
+    //   e.target.previousSibling.className = 'inputOnFocus';
+
+    const inputName = e.target.name;
+    setIsFocused((prevState) => ({
+      ...prevState,
+      [inputName]: true,
+    }));
   };
   const focusOut = (e: any) => {
-    if (e.target.value == '') {
-      e.target.previousSibling.className = 'inputOffFocus';
-      e.target.nextSibling.className = 'errorMessage';
-    }
+    // if (e.target.value == '') {
+    //   e.target.previousSibling.className = 'inputOffFocus';
+    //   e.target.nextSibling.className = 'errorMessage';
+    // }
+
+    const inputName = e.target.name;
+    setIsFocused((prevState) => ({
+      ...prevState,
+      [inputName]: false,
+    }));
   };
 
   const SignUpSuccessPage = () => {
@@ -112,9 +103,9 @@ const SignUp = () => {
         </h1>
         <p>RETFLX에서 제공하는 모든 서비스를 이용할 수 있습니다.</p>
         <div>
-          <Link to='/SignIn'>
+          {/* <Link to='/SignIn'>
             <button className='signInBtn'>로그인 하러가기</button>
-          </Link>
+          </Link> */}
           <Link to='/'>
             <button className='homeBtn'>홈으로 이동</button>
           </Link>
@@ -141,7 +132,7 @@ const SignUp = () => {
                 <label onFocus={focusIn} onBlur={focusOut}>
                   <span className='inputOffFocus'>이메일</span>
                   <input type='email' name='email' onChange={onChangeEmail} value={email} />
-                  <p>규칙에 맞는 이메일 주소를 입력해주세요</p>
+                  {isEmailError ? <p>규칙에 맞는 이메일 주소를 입력해주세요</p> : null}
                 </label>
 
                 <label onFocus={focusIn} onBlur={focusOut}>
@@ -150,9 +141,9 @@ const SignUp = () => {
                     type='password'
                     name='password'
                     value={password}
-                    onChange={onChangePassword}
+                    onChange={passwordLength}
                   />
-                  <p>비밀번호는 8자리 이상으로 입력해주세요.</p>
+                  {isPasswordError ? <p>비밀번호는 8자리 이상으로 입력해주세요.</p> : null}
                 </label>
 
                 <label onFocus={focusIn} onBlur={focusOut}>
@@ -163,7 +154,9 @@ const SignUp = () => {
                     value={passwordCheck}
                     onChange={onChangePasswordCheck}
                   />
-                  <p>비밀번호와 비밀번호 확인이 일치하지 않습니다.</p>
+                  {isPasswordCheckError ? (
+                    <p>비밀번호와 비밀번호 확인이 일치하지 않습니다.</p>
+                  ) : null}
                 </label>
 
                 <button type='submit' onClick={onSubmit}>
@@ -175,12 +168,12 @@ const SignUp = () => {
               </div>
             </Form>
             <div className='suggestion'>
-              <p>
+              {/* <p>
                 이미 회원이신가요?
                 <Link to='/SignIn'>
                   <span>로그인 하러가기</span>
                 </Link>
-              </p>
+              </p> */}
             </div>
           </>
         )}
