c1494d9b54f3513cd8a25fc0e8c5536446a04ce2
dordos <pdhssf@gmail.com>
2023-04-05 23:50:35 +0900

    address update

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import MenuBar from './components/ui/MenuBar';
 
 const queryClient = new QueryClient();
 function App() {
   console.log('d');
   return (
-    <QueryClientProvider client={queryClient}>
+    // <QueryClientProvider client={queryClient}>
+    <>
+      <MenuBar />
       <Outlet />
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
+    </>
+    // <ReactQueryDevtools initialIsOpen={true} />
+    // </QueryClientProvider>
   );
 }
 
